eff5ce4d3f4913fd2da2c45006937b976851e7f2
foxdefox-wq <bluefoxbrownbear@gmail.com>
2026-02-17 21:16:04 -0800

    update

--- Sources/MyProject/main.swift
+++ Sources/MyProject/main.swift
@@ -1,3 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// yoo
+import Raylib
+
+Raylib.initWindow(100, 100, "Cool")
+Raylib.setTargetFPS(60)
+
+extension Vector3 {
+    init(_ x: Float, _ y: Float, _z: Float) {
+        self.init()
+    }
+}
+
+while Raylib.windowShouldClose == false {
+    var camera = Camera3D()
+    Raylib.beginMode3D(Camera)
+    Raylib.clearBackground(.rayWhite)
+}
