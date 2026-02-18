158aa249defc5f05dc31019d974e07963d865812
foxdefox-wq <bluefoxbrownbear@gmail.com>
2026-02-17 21:00:16 -0800

    update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MyProject",
     dependencies: [
-        .package(url: "https://github.com", branch: "master")
+        .package(url: "https://github.com/STREGAsGate/Raylib", branch: "master")
     ],
     targets: [
         .executableTarget(
